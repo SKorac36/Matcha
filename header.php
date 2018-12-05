0db2de5b3a2a2c20f6e5c6cd733c96abca41d4b7
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2018-12-05 13:49:42 +0200

    little bit

--- header.php
+++ header.php
@@ -17,6 +17,7 @@ $index = "index.php";
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="stylesheet.css">
 <!-- <link rel="stylesheet" href="style.css"> -->
 <body>
 <div class="container-fluid w3-pink">
--- profile.php
+++ profile.php
@@ -2,7 +2,10 @@
     require_once('header.php');
     if (isset($_SESSION) || isset($_SESSION['uid']))
     {
-
+        $uid = $_SESSION['uid'];
+        $query = "SELECT * FROM Matcha.Profiles WHERE id=?";
+        $sql = $conn->prepare($query);
+        $sql->execute([$uid]);
     }
 
 ?>
