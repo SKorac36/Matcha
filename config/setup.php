1684fbd27a22467b28ac13deb1c1f5412bc50783
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2018-12-11 11:54:45 +0200

    updated tables

--- config/setup.php
+++ config/setup.php
@@ -2,7 +2,7 @@
 include_once('database.php');
 include_once('connect.php');
 $db = "CREATE DATABASE IF NOT EXISTS Matcha";
-$usrs = "CREATE TABLE Matcha.Users (
+$usrs = "CREATE TABLE IF NOT EXISTS Matcha.Users (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(1000) NOT NULL,
     username VARCHAR(30) NOT NULL,
@@ -11,32 +11,34 @@ $usrs = "CREATE TABLE Matcha.Users (
     first_name VARCHAR(255) NOT NULL,
     regdate TIMESTAMP 
     )";
-// $pics = "CREATE TABLE Camagru.Pics (
-//     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//     userid VARCHAR(30) NOT NULL,
-//     picname VARCHAR(100) NOT NULL,
-//     moddate TIMESTAMP,
-//     likes INT(255) DEFAULT 0
-//     )";
-// $comments = "CREATE TABLE Camagru.Comments (
-//     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//     ownerid INT(6) NOT NULL,
-//     commenterid INT(6) NOT NULL,
-//     comm VARCHAR(1000),
-//     img_id INT(6) NOT NULL,
-//     time TIMESTAMP)";
- $profile = "CREATE TABLE Matcha.Profiles (
+ $profile = "CREATE TABLE IF NOT EXISTS Matcha.Profiles (
      id INT(6) PRIMARY KEY NOT NULL,
      age INT(3) NOT NULL,
     gender VARCHAR(1000) NOT NULL,
     preference VARCHAR(1000) DEFAULT 'bisexual',
-    bio TEXT DEFAULT 'Enter you bio here, you can even use tags to describe yourself. Just prefix them with a hashtag.'
+    bio VARCHAR(1000),
+    tags VARCHAR(1000),
+    -- latitude TEXT NOT NULL,
+    -- longitude TEXT NOT NULL,
+    views INT(255) DEFAULT 0,
+    likes INT(255) DEFAULT 0,
+    regdate TIMESTAMP
      )";
+$images = "CREATE TABLE IF NOT EXISTS Matcha.Images (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    userid INT(6) NOT NULL,
+    path VARCHAR(1000) NOT NULL
+    )";
+$tags = "CREATE TABLE IF NOT EXISTS Matcha.Tags (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    tag VARCHAR(30) NOT NULL,
+    count INT(60) DEFAULT 0
+    )";
 $conn->query($db);
-// $conn->query($usrs);
- $conn->query($profile);
-// $conn->query($comments);
-// $conn->query($likes);
+$conn->query($usrs);
+$conn->query($profile);
+$conn->query($images);
+$conn->query($tags);
 echo "<script type='text/javascript'>
 	alert('Successfully created database');
 	window.location.href = '../index.php'; 
