994424b002df56a68a4ca09a4edc46762f19989f
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2018-12-12 15:19:22 +0200

    slight css, added profile picture

--- profile.php
+++ profile.php
@@ -7,22 +7,27 @@
         $sql = $conn->prepare($query);
         $sql->execute([$uid]);
         $profile = $sql->fetch();
+        $query = "SELECT *FROM Matcha.Images WHERE id=?";
+        $sql = $conn->prepare($query);
+        $sql->execute([$uid]);
+        $pic = $sql->fetch();
         $first_name = $profile['first_name'];
         $last_name = $profile['last_name'];
         $bio = $profile['bio'];
         $age = $profile['age'];
+        $path = $pic['path'];
     }
 
     // var_dump($profile);
 ?>
 <html>
 <div class="container-fluid w3-light-grey">
-  <img src="stock.png"> <br>
-  <?php
-
+<div align="center"><?php  
+    echo '<img src="'.$path.'"</img>';
     echo '<p>'.$first_name.' '.$last_name.' '.$age.' location:<br>'.$bio.'
     </p>';
   ?>
+  </div>
 </div>
 <?php
     include('footer.php');
