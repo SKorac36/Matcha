e63c2c753732a2bbf5253841e8f80af64ca265d2
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2018-12-12 15:18:50 +0200

    tried to get to work, have an issue sending the info

--- geolocation.php
+++ geolocation.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <p>Click the button to get your coordinates.</p>
 
 <button onclick="getLocation()">Try It</button>
+<form method="post" class="form" action="store_location.php"><button onclick="drukHom()">Druk hom</button></form>
+
+
 
 <p id="demo"></p>
 
 <script>
 var x = document.getElementById("demo");
-
+var ll = [];
+function drukHom() {
+  var XHR = new XMLHttpRequest();
+  XHR.onreadystatechange = function(status, state) {
+  }
+  XHR.open('POST','store_location.php');
+  XHR.send(ll);
+}
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -20,6 +29,13 @@ function getLocation() {
 }
 
 function showPosition(position) {
+
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  
+  ll[0] = long;
+  ll[1] = lat;
+  console.log(ll);
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 }
