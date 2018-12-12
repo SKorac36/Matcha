1b0cce1df387b68185f4cfca368364e842384e87
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2018-12-12 15:21:00 +0200

    allow uploads

---
+++ upload_image.php
@@ -0,0 +1,16 @@
+<?php
+    include 'config/connect.php';
+    session_start();
+if (isset($_SESSION) && !empty($_SESSION['uid']))
+{
+    $img_dir = "imgs/";
+    $img_name = basename($_FILES['file']['name']);
+    $new_name = uniqid();
+    $img_file_type = ".". strtolower(pathinfo($img_dir . $_FILES['file']['name'], PATHINFO_EXTENSION));
+    $target = $img_dir . $new_name . $img_file_type;
+    move_uploaded_file($_FILES['file']['tmp_name'], $target);
+    $query = $conn->prepare("INSERT INTO Matcha.images (userid, path) VALUES (?,?)");
+    $query->execute([$_SESSION['uid'],$target]);
+    header("location: user_setup.php");
+}
+?>
