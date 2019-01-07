fac71f6e950fafa1426d03ccf9f2069764cc3e8f
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2019-01-07 11:37:52 +0200

    calls like function

--- like.php
+++ like.php
@@ -1,8 +1,12 @@
 <?php
 
-// if (isset($_GET()))
-
-
-
+require_once('functions.php');
+require_once('config/connect.php');
+if (isset($_GET['id1']) && isset($_GET['id2']))
+{
+    $liker = $_GET['id1'];
+    $likee = $_GET['id2'];
+    like($likee, $liker, $conn);
+}
 
 ?>
