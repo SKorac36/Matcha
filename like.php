a138865fd0a6bc24d1120d85ab4f45cc78347079
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2019-01-07 13:39:42 +0200

    added liking system

--- like.php
+++ like.php
@@ -4,9 +4,10 @@ require_once('functions.php');
 require_once('config/connect.php');
 if (isset($_GET['id1']) && isset($_GET['id2']))
 {
-    $liker = $_GET['id1'];
-    $likee = $_GET['id2'];
-    like($likee, $liker, $conn);
+    $liker = (int)$_GET['id1'];
+    $likee = (int)$_GET['id2'];
+    like($liker, $likee, $conn);
+    alert('Nice you like them', 'profile.php?profile_id='.$likee);
 }
 
 ?>
