7be26cd4d72d7c417a9df658f7c4e2d713d7ad14
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2019-01-14 14:11:07 +0200

    minor changes

--- block.php
+++ block.php
@@ -1,13 +1,14 @@
 <?php
  
  require_once('./config/connect.php');
+ require_once('functions.php'); 
  
  if(isset($_GET['id1']) && isset($_GET['id2']))
  {
-        $blocker = $_GET['id1'];
-        $blockee = $_GET['id2'];
-        $query = "INSERT INTO Matcha.Blocks SET blocker=? AND blockee=?";
-        $sql = $conn->query($query);
+        $blocker = (int)$_GET['id1'];
+        $blockee = (int)$_GET['id2'];
+        $query = "INSERT INTO Matcha.blocks (blocker,blockee) VALUES (?,?)";
+        $sql = $conn->prepare($query);
         $sql->execute([$blocker, $blockee]);
         alert("You have successfully blocked the pest, they will be unable to see your profile", "index.php");
  }
