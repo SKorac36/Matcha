b25c63139a73a01c4f6733d6a57b9816319b1cff
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2019-01-15 15:23:44 +0200

    allowed for sort based on check boxes

--- browse_profiles.php
+++ browse_profiles.php
@@ -2,6 +2,7 @@
 
 require_once('header.php');
 
+
 if (isset($_SESSION) && !empty($_SESSION['uid']))
 {
     $query = "SELECT * FROM Matcha.Profiles WHERE id=?";
@@ -11,11 +12,28 @@ if (isset($_SESSION) && !empty($_SESSION['uid']))
     $pref = $user['preference'];
     $gender = $user['gender'];
 }
+$option = 'id';
+if (isset($_POST['submit']))
+{ 
+    // var_dump($_POST);
+
+    if (isset($_POST['option']))
+        $option = $_POST['option'];
+    var_dump($_POST['submit']);
+}
 // $blocks = getBlocks($conn, $_SESSION['uid']);
-$matches = matching($pref, $gender, $conn);
+$matches = matching($pref, $gender, $conn, $option);
 // var_dump($blocks);
+var_dump($_POST['submit']);
 ?>
-  <table padding="15px">
+<form class="form" action="browse_profiles.php" method="post">
+  <input type="radio" name="option" value="age" /> Age
+  <input type="radio" name="option" value="location" /> Location
+  <input type="radio" name="option" value="fame_rating" /> Fame Rating<br>
+  <input type="submit" class="btn" name="submit" value="OK"/>
+  
+</form>
+  <table>
                 <?php
             
                 if (!$matches)
@@ -27,7 +45,7 @@ $matches = matching($pref, $gender, $conn);
                     if ($matches)
                     {
                         if (!(checkBlocks($conn, $_SESSION['uid'], $id)))
-                            echo '<tr<td><a href="profile.php?id='.$id.'"><img src="'.$pic.'" height="300" width="400"/></a></td></tr>';
+                            echo '<tr<td><a href="profile.php?id='.$id.'"><img src="'.$pic.'" height="300" width="400" hspace="20"/></a></td></tr>';
                     }
                 }
                 ?>
