98b3579ba73d2e19eb7588365bc2b781647b2b13
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2019-01-18 11:49:43 +0200

    check for profile picture

--- like.php
+++ like.php
@@ -6,8 +6,12 @@ if (isset($_GET['id1']) && isset($_GET['id2']))
 {
     $liker = (int)$_GET['id1'];
     $likee = (int)$_GET['id2'];
-    like($liker, $likee, $conn);
-    alert('Nice you like them', 'profile.php?id='.$likee);
+    if (picCheck($liker, $conn) == true)
+    {
+        like($liker, $likee, $conn);
+        alert('Nice you like them', 'profile.php?id='.$likee);
+    }
+    else
+        alert('You need to upload an image','upload_images.php');
 }
-
 ?>
