a592a80dfdc1720042541b97c5a28fea1c4f67ef
Stefan KORAC <skorac@e4r14p10.wethinkcode.co.za>
2019-01-22 15:00:28 +0200

    now display suggestions

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
 <?php
 
 require_once('header.php');
-echo hash('whirlpool', '1234$ABCs');
+
+
+if (isset($_SESSION) && !empty($_SESSION['uid']))
+{
+    $query = "SELECT * FROM Matcha.Profiles WHERE id=?";
+    $sql = $conn->prepare($query);
+    $sql->execute([$_SESSION['uid']]);
+    $user = $sql->fetch();
+    $pref = $user['preference'];
+    $gender = $user['gender'];
+    $latitude = $user['latitude'];
+    $longitude = $user['longitude'];
+    $tags = unserialize($user['tags']);
+    $fr = $user['fame_rating'];
+}
+$option = 'id';
+if (isset($_POST['option']))
+        $option = $_POST['option'];
+$matches = suggestions($pref, $gender,$latitude,$longitude, $tags, $conn, $option, $fr);
 ?>
-<html>
-<div id="wrapper">
-<h1>Home</h1>
-</div>
-<?php
-include('footer.php');
-?>
-</html>
+  <table>
+                <?php
+            
+                if (!$matches)
+                    echo "<h1>Whoops nothing here, no one here</h1>";
+                else foreach($matches as $row)
+                {
+                    $pic = $row['profile_pic'];
+                    $id = $row['id'];
+                    if ($matches)
+                    {
+                        if (!(checkBlocks($conn, $_SESSION['uid'], $id)))
+                            echo '<tr<td><a href="profile.php?id='.$id.'"><img src="'.$pic.'"hspace="20"/></a></td></tr>';
+                    }
+                }
+                
+                ?>
+        </table>
+        <?php
+        include_once('footer.php');
+        ?>
